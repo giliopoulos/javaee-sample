a38fe512d2d29f46d5525b6e4f78148f06eedcfc
heliop <grigoris.csd@gmail.com>
2018-04-10 20:36:25 +0300

    Removes injection that prevented server from starting

--- javee-sample-web/src/main/java/gr/giliopoulos/sample/web/api/EmployeeResources.java
+++ javee-sample-web/src/main/java/gr/giliopoulos/sample/web/api/EmployeeResources.java
@@ -1,27 +1,20 @@
 package gr.giliopoulos.sample.web.api;
 
-import gr.giliopoulos.sample.domain.Employee;
-import gr.giliopoulos.sample.services.jpa.EmployeeRepository;
-
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("employees")
 @Produces(MediaType.APPLICATION_JSON)
 public class EmployeeResources {
 
-    @Inject
-    private EmployeeRepository employeeRepository;
-
-
     @GET
-    public List<Employee> findAll(){
-        return employeeRepository.findAll();
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response findAll(){
+        return Response.ok().entity(new String("hello")).build();
     }
 }
