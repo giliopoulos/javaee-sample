0b3654836c2f0402b5d96f6d9678e1cff0257583
Grigoris Iliopoulos <grigoris.csd@gmail.com>
2018-05-11 18:04:49 +0300

    Adds tests for rest services

--- javee-sample-web/src/test/java/gr/giliopoulos/sample/web/api/employeresources/EmployeeResourcesTest.java
+++ javee-sample-web/src/test/java/gr/giliopoulos/sample/web/api/employeresources/EmployeeResourcesTest.java
@@ -5,13 +5,31 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 
 @Category(Rest.class)
 public class EmployeeResourcesTest {
 
     @Test
-    public void testGoogleIsUp(){
-        given().when().get("http://www.google.com").then().statusCode(200);
+    public void testEmployeesEndpointIsUp() {
+        given().
+        when().
+                get("http://localhost:8081/javee-sample-web/api/employees").
+        then().
+                statusCode(200);
     }
+
+    @Test
+    public void getEmployeeWithId10001_exists() {
+        given().
+                pathParam("id", 10001).
+        when().
+                get("http://localhost:8081/javee-sample-web/api/employees/{id}").
+        then().
+                body("id",equalTo(10001)).
+                body("firstName", equalTo("Georgi"));
+    }
+
 
 }
